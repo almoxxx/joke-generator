44e86fa4f45e9beae5463a424d9197405509bf1c
almoxxx <120190420+almoxxx@users.noreply.github.com>
2024-02-10 18:13:36 +0100

    added typewriter and js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 function generateJoke(event) {
   event.preventDefault();
-  alert("I'm working!");
-  let jokeElement = document.querySelector("#joke");
-  jokeElement.innerHTML =
-    "Why don't scientists trust atoms? Because they make up everything!";
+
+  new Typewriter("#joke", {
+    strings:
+      "Why don't scientists trust atoms? Because they make up everything!",
+    autoStart: true,
+  });
 }
 
 let jokeFormElement = document.querySelector("#joke-generator-form");
